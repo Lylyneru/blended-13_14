9223a7955a26394de612b68297cfd7952f45842c
LiliiaOsinska <lo.lihcka@gmail.com>
2024-09-29 16:06:04 +0200

    add task 4

--- js/task-1.js
+++ js/task-1.js
@@ -57,10 +57,18 @@
 //   у якому перше входження підрядка буде видалено.Якщо підрядок не знайдено,
 //     функція повинна повертати оригінальний рядок.
 
-// Приклади використання:
-console.log(removeSubstring("Hello, world!", "world")); // "Hello, !"
-console.log(removeSubstring("Hello, world!", "JavaScript")); // "Hello, world!"
-console.log(removeSubstring("Hello, world!", "Hello")); // "
+// function removeSubstring(str, substr) {
+//     const index = str.indexOf(substr);
+//     if(index === -1){
+//         return str
+//     }
+//     return str.slice(0, index)+ str.slice(index + substr.length)
+// }
+
+// // Приклади використання:
+// console.log(removeSubstring("Hello, world!", "world")); // "Hello, !"
+// console.log(removeSubstring("Hello, world!", "JavaScript")); // "Hello, world!"
+// console.log(removeSubstring("Hello, world!", "Hello")); // ", world!"
 
 5;
 
